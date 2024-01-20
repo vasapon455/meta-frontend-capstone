e209487c205e25c6075c74c5d468f0b403bb64b2
Vasapon Opaswatanakul <vasapon1996@gmail.com>
2024-01-21 02:23:11 +0700

    setupTest

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,20 +1,36 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../../Pages/HomePage/HomePage";
-import { useReducer } from "react";
+import { useReducer, useState, useEffect } from "react";
 import BookingPage from "../../Pages/BookingPage/BookingPage";
 import { fetchAPI, submitAPI } from "../../utils/fakeAPI";
 import ConfirmedBooking from "../../Pages/ConfirmedBooking/ConfirmedBooking";
 import { useNavigate } from "react-router-dom";
 
-export default function Main({ review }) {
+export default function Main() {
+  const [review, setReview] = useState();
+
+  const fetchData = () => {
+    try {
+      fetch("https://randomuser.me/api/?results=4")
+        .then((response) => response.json())
+        .then((data) => setReview(data.results));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   const navigate = useNavigate();
+
   const initializeTimes = () => {
     const date = new Date();
     return [...fetchAPI(date)];
   };
-  const updateTime = (availableTimes, date) => {
+  const updateTime = (availableTime, date) => {
     const response = fetchAPI(new Date(date));
-    return response.length !== 0 ? response : availableTimes;
+    return response.length !== 0 ? response : availableTime;
   };
 
   const submitForm = (formData) => {
