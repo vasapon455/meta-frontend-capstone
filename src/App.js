565f342d012b482c033868660b4b263c1bc01b49
Vasapon Opaswatanakul <vasapon1996@gmail.com>
2024-01-21 02:24:09 +0700

    Rearrange Components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import "./App.css";
-import { useEffect, useState } from "react";
 import Header from "./Layout/Header/Header";
 import Main from "./Layout/Main/Main";
 import Footer from "./Layout/Footer/Footer";
 
 function App() {
-  const [review, setReview] = useState();
-
-  const fetchData = () => {
-    try {
-      fetch("https://randomuser.me/api/?results=4")
-        .then((response) => response.json())
-        .then((data) => setReview(data.results));
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <>
       <Header />
-      <Main review={review} />
+      <Main />
       <Footer />
     </>
   );
